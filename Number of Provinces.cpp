146589a834135997c21e5f2737de7548bf8c0eff
awanishkrai <awanishrai420@gmail.com>
2026-01-25 01:04:21 +0530

    practice

---
+++ Number of Provinces.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    void dfs(vector<vector<int>>& isConnected, vector<bool>& visited, int i) {
+        visited[i] = true;
+        for(int j = 0; j < isConnected[i].size(); j++) {
+            if(isConnected[i][j] == 1 && !visited[j]) {
+                dfs(isConnected, visited, j);   
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected) {
+        int n = isConnected.size();
+        vector<bool> visited(n, false);
+        int count = 0;
+
+        for(int i = 0; i < n; i++) {
+            if(!visited[i]) {
+                count++;
+                dfs(isConnected, visited, i);
+            }
+        }
+        return count;   
+    }
+};
