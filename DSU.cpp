127a2e08426af647b2956be5a79b04c770e3f929
awanishkrai <awanishrai420@gmail.com>
2026-02-19 14:56:13 +0530

    practice

--- DSU.cpp
+++ DSU.cpp
@@ -5,3 +5,22 @@
 
 //union by size->smaller to larger
 //path compression technique ensure  find operartion in O(1);
+class dsu{
+    int n;
+    vector<int>par(n);
+    vector<int>size(n);
+    dsu(int n){
+        this->n=n;
+        parent.resize(n);
+        size.resize(n);
+        for(int i=0;i<n;i++){
+            parent[i]=i;
+        }
+    }
+    int find(int i){
+        if(parent[i]==i)return i;
+        //path compression 
+        return parent[x]= find(parent[x]);
+    }
+
+}
